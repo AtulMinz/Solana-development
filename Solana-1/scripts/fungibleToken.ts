be5fd77d1184d8dea5da2d99f28b4918df1add97
Atulya Minz <atulminz.jsr@gmail.com>
2024-07-22 15:28:10 +0530

    Tweaks

--- Solana-1/scripts/fungibleToken.ts
+++ Solana-1/scripts/fungibleToken.ts
@@ -1,1 +1,4 @@
 import { createMint } from "@solana/spl-token";
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
