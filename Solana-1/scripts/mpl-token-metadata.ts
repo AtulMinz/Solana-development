46f3e058833438171d2eb71cb1ecc38369279c35
Atulya Minz <atulminz.jsr@gmail.com>
2024-07-23 14:49:30 +0530

    Completed token metadata script

--- Solana-1/scripts/mpl-token-metadata.ts
+++ Solana-1/scripts/mpl-token-metadata.ts
@@ -10,6 +10,7 @@ import {
   Connection,
   Keypair,
   PublicKey,
+  sendAndConfirmTransaction,
   SystemProgram,
   Transaction,
 } from "@solana/web3.js";
@@ -23,6 +24,7 @@ import {
   getTokenMetadata,
   TYPE_SIZE,
   LENGTH_SIZE,
+  getMetadataPointerState,
 } from "@solana/spl-token";
 import { secretKey } from "./utils";
 
@@ -68,7 +70,86 @@ async function tokenWithMetadata() {
 
   const createAccountInstruction = SystemProgram.createAccount({
     fromPubkey: user.publicKey,
+    newAccountPubkey: mint,
+    space: mintLen,
+    lamports,
+    programId: TOKEN_2022_PROGRAM_ID,
   });
+
+  const initializeMetadataPointerInstruction =
+    createInitializeMetadataPointerInstruction(
+      mint,
+      updateAuthority,
+      mint,
+      TOKEN_2022_PROGRAM_ID
+    );
+
+  const initializeMintInstruction = createInitializeMintInstruction(
+    mint,
+    decimals,
+    mintAuthority,
+    null,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  const initializeMetadataInstruction = createInitializeInstruction({
+    programId: TOKEN_2022_PROGRAM_ID,
+    metadata: mint,
+    updateAuthority: updateAuthority,
+    mint: mint,
+    mintAuthority: mintAuthority,
+    name: metaData.name,
+    symbol: metaData.symbol,
+    uri: metaData.uri,
+  });
+
+  const updateFieldInstruction = createUpdateFieldInstruction({
+    programId: TOKEN_2022_PROGRAM_ID,
+    metadata: mint,
+    updateAuthority: updateAuthority,
+    field: metaData.additionalMetadata[0][0], //key
+    value: metaData.additionalMetadata[0][1], //value
+  });
+
+  transaction = new Transaction().add(
+    createAccountInstruction,
+    initializeMetadataPointerInstruction,
+    // note: the above instructions are required before initializing the mint
+    initializeMintInstruction,
+    initializeMetadataInstruction,
+    updateFieldInstruction
+  );
+
+  transactionSignature = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [user, mintKeypair]
+  );
+
+  console.log(
+    "\nCreate Mint Account:",
+    `https://explorer.solana.com/address/${mint}?cluster=devnet`
+  );
+
+  console.log(mint);
+
+  //------------------------------------------READ METADATA FROM MINT ACCOUNT----------------------------------------------
+
+  //Retrieve mint information
+  const mintInfo = await getMint(
+    connection,
+    mint,
+    "confirmed",
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  const metadataPointer = getMetadataPointerState(mintInfo);
+  console.log("\nMetadata Pointer: ", JSON.stringify(metadataPointer, null, 2));
+
+  //Read the log of the metadata state
+  const metadata = await getTokenMetadata(connection, mint);
+
+  console.log("\nMetadata: ", JSON.stringify(metadata, null, 2));
 }
 
 tokenWithMetadata();
