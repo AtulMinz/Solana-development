bfa7328b704addc4b0b8a19cd22dd0cad4bdac2d
Atulya Minz <atulminz.jsr@gmail.com>
2024-07-23 13:03:34 +0530

    Update metadata token script

--- Solana-1/scripts/mpl-token-metadata.ts
+++ Solana-1/scripts/mpl-token-metadata.ts
@@ -1,5 +1,29 @@
-import {} from "@solana/spl-token-metadata";
-import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js";
+import {
+  createInitializeInstruction,
+  createUpdateFieldInstruction,
+  createRemoveKeyInstruction,
+  pack,
+  TokenMetadata,
+} from "@solana/spl-token-metadata";
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import {
+  ExtensionType,
+  TOKEN_2022_PROGRAM_ID,
+  createInitializeMintInstruction,
+  getMintLen,
+  createInitializeMetadataPointerInstruction,
+  getMint,
+  getTokenMetadata,
+  TYPE_SIZE,
+  LENGTH_SIZE,
+} from "@solana/spl-token";
 import { secretKey } from "./utils";
 
 async function tokenWithMetadata() {
@@ -7,7 +31,44 @@ async function tokenWithMetadata() {
 
   const user = Keypair.fromSecretKey(secretKey);
 
-  console.log(`We've loaded the keypair! Public key is: ${user.publicKey}`);
+  console.log(`We've loaded the keypair! 🔑 Public key is: ${user.publicKey}`);
+
+  let transaction: Transaction;
+
+  let transactionSignature: string;
+
+  const mintKeypair = Keypair.generate();
+
+  const mint = mintKeypair.publicKey;
+
+  const decimals = 2;
+
+  const mintAuthority = user.publicKey;
+
+  const updateAuthority = user.publicKey;
+
+  const metaData: TokenMetadata = {
+    updateAuthority: updateAuthority,
+    mint: mint,
+    name: "FAITH",
+    symbol: "YSHU",
+    uri: "https://raw.githubusercontent.com/AtulMinz/Solana-development/main/Solana-1/scripts/metadata/metadata.json",
+    additionalMetadata: [["description", "Faith in GOD"]],
+  };
+
+  const metadataExtension = TYPE_SIZE + LENGTH_SIZE;
+
+  const metadataLen = pack(metaData).length;
+
+  const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+
+  const lamports = await connection.getMinimumBalanceForRentExemption(
+    mintLen + metadataExtension + metadataLen
+  );
+
+  const createAccountInstruction = SystemProgram.createAccount({
+    fromPubkey: user.publicKey,
+  });
 }
 
 tokenWithMetadata();
